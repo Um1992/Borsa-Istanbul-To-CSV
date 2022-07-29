9a5c44d2ab4a77b4e81eeb0f9d3513568c614ad4
Umut <rdizzle1992@gmail.com>
2022-07-29 22:14:20 +0300

    Example History List

---
+++ Download All History.py
@@ -0,0 +1,10 @@
+from pandas_datareader.data import DataReader
+import pandas as pd
+
+df = pd.read_csv('Tickers.csv').to_numpy()
+
+for ticker in df:
+    data = DataReader(ticker, 'yahoo', start='2020-01-01', end='2022-07-29')
+    filename = "History Data/{}.csv".format(ticker)
+    data.to_csv(filename)
+    print(ticker+"Done")
