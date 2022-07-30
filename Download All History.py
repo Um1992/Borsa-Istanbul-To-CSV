e40eedb8cae26515804c6ea7981d3b046dab73e5
Umut <rdizzle1992@gmail.com>
2022-07-30 19:25:57 +0300

    merge added

--- Download All History.py
+++ Download All History.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv('Tickers.csv').to_numpy()
 
 for ticker in df:
-    data = DataReader(ticker, 'yahoo', start='2020-01-01', end='2022-07-29')
+    data = DataReader(ticker, 'yahoo', start='2020-01-01', end='2022-07-29')["Close"]
     filename = "History Data/{}.csv".format(ticker)
     data.to_csv(filename)
-    print(ticker+"Done")
+    print(ticker+" Done")
